f6d8fdefce774e19933b72661e66bbeb990ae60a
SafaeQ <safaedani009@gmail.com>
2021-12-24 15:42:35 +0100

    writeheader to ok

--- assets/controllers/todosController.js
+++ assets/controllers/todosController.js
@@ -4,6 +4,10 @@ const Todo = require('../models/todosModel')
 async function getTodos(req, res) {
     try {
         const todo = await Todo.getALLTodos()
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        res.end(JSON.stringify(todo))
     } catch (error) {
 
     }
