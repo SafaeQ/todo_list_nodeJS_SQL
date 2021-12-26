c69e3528ce50cbd4ec50af4e4bfdc105e08d73b7
SafaeQ <safaedani009@gmail.com>
2021-12-26 14:48:01 +0100

    add function update todo

--- assets/controllers/todosController.js
+++ assets/controllers/todosController.js
@@ -54,6 +54,18 @@ async function deleteTodo(req, res, id) {
         res.end('Route Not Found')
     }
 }
+async function updateTodoId(req, res, id) {
+    try {
+        const todo = await Todo.updateTodoById(id)
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        res.end(JSON.stringify(todo))
+    } catch (error) {
+        console.error(error)
+        res.end('Route Not Found')
+    }
+}
 module.exports = {
     getTodos,
     getTodo,
