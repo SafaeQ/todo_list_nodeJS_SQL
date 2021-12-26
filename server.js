a514a0f2cfb1b0ce59eba1c39d4981ba68a27947
SafaeQ <safaedani009@gmail.com>
2021-12-26 14:51:55 +0100

    edit path of controller in server

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const {
     getTodos,
     getTodo,
     createTodo,
-    deleteTodo
+    deleteTodo,
+    updateTodoId
 } = require('./assets/controllers/todosController')
 const hostname = '127.0.0.1';
 const PORT = 3000;
@@ -18,54 +19,58 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
 
-            switch (req.method) {
-                case 'GET':
-                    if (req.url === '/projects') {
-                        return getProjects(req, res);
-                    } else if (req.url.match(/\/projects\/(\d+)/) != null) {
-                        // console.log('fff');
-                        const id = req.url.match(/\/projects\/(\d+)/)[1]
-                        return getProjectId(req, res, id);
-                    }
-                    if (req.url === '/todos') {
-                        return getTodos(req, res);
-                    } else if (req.url.match(/\/todos\/(\d+)/) !== null) {
-                        const id = req.url.match(/\/todos\/(\d+)/)[1]
-                        return getTodo(req, res, id)
-                    }
-                    break;
-                case 'POST':
-                    if (req.url === '/project') {
-                        return createProject(req, res)
-                    } else if (req.url === '/todo') {
-                        return createTodo(req, res)
-                    }
+    switch (req.method) {
+        case 'GET':
+            if (req.url === '/projects') {
+                return getProjects(req, res);
+            } else if (req.url.match(/\/projects\/(\d+)/) != null) {
+                // console.log('fff');
+                const id = req.url.match(/\/projects\/(\d+)/)[1]
+                return getProjectId(req, res, id);
+            }
+            if (req.url === '/todos') {
+                return getTodos(req, res);
+            } else if (req.url.match(/\/todos\/(\d+)/) !== null) {
+                const id = req.url.match(/\/todos\/(\d+)/)[1]
+                return getTodo(req, res, id)
+            }
+            break;
+        case 'POST':
+            if (req.url === '/project') {
+                return createProject(req, res)
+            } else if (req.url === '/todo') {
+                return createTodo(req, res)
+            }
 
-                    break;
-                case 'DELETE':
-                    if (req.url.match(/\/projects\/(\d+)/) != null) {
-                        const id = req.url.match(/\/projects\/(\d+)/)[1]
-                        res.end('item deleted successfully')
-                        return deleteProjectId(req, res, id);
-                    } else if (req.url.match(/\/todos\/(\d+)/) != null) {
-                        const id = req.url.match(/\/todos\/(\d+)/)[1]
-                        res.end('todo deleted successfully')
-                        return deleteTodo(req, res, id);
-                    }
-                    break;
-                case 'PUT':
-                    if (req.url.match(/\/projects\/(\d+)/) != null) {
-                        const id = req.url.match(/\/projects\/(\d+)/)[1]
-                        return updateProjectId(req, res, id);
-                    } else if (req.url.match(/\/))
-                            break;
-                            default:
-                            res.statusCode = 404; res.end('Route Not Found');
-                        }
-                    })
+            break;
+        case 'DELETE':
+            if (req.url.match(/\/projects\/(\d+)/) != null) {
+                const id = req.url.match(/\/projects\/(\d+)/)[1]
+                res.end('item deleted successfully')
+                return deleteProjectId(req, res, id);
+            } else if (req.url.match(/\/todos\/(\d+)/) != null) {
+                const id = req.url.match(/\/todos\/(\d+)/)[1]
+                res.end('todo deleted successfully')
+                return deleteTodo(req, res, id);
+            }
+            break;
+        case 'PUT':
+            if (req.url.match(/\/projects\/(\d+)/) != null) {
+                const id = req.url.match(/\/projects\/(\d+)/)[1]
+                return updateProjectId(req, res, id);
+            } else if (req.url.match(/\/todos\/(\d+)/) != null) {
+                const id = req.url.match(/\/todos\/(\d+)/)[1]
+                return updateTodoId(req, res, id);
+            }
+            break;
+        default:
+            res.statusCode = 404;
+            res.end('Route Not Found');
+    }
+})
 
 
 
-            server.listen(PORT, () => {
-                console.log(`server listening on http://${hostname}:${PORT}/`);
-            });
+server.listen(PORT, () => {
+    console.log(`server listening on http://${hostname}:${PORT}/`);
+});
